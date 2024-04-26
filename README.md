09e671499fe1bd050047bb64b5a00c67f13dcac4
jisookh <jisookhong5@gmail.com>
2024-04-26 16:49:44 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# 자료구조 스터디
+* 교재: 한국외대 신찬수 교수의 강의 ["자료구조 - Data Structures with Python"](https://www.youtube.com/watch?v=PIidtIBCjEg&list=PLsMufJgu5933ZkBCHS7bQTx0bncjwi4PK&pp=iAQB) (2020, Spring)
+
+## 목차
+1. 자료구조 과목 소개
+2. 자료구조와 알고리즘
+3. 알고리즘 시간복잡도 1
+4. 알고리즘 시간복잡도 2
---
+++ 연습문제/gcd.py
@@ -0,0 +1,23 @@
+def gcd_sub(a,b): 
+    while a != 0 and b != 0:
+        if a > b: a = a-b
+        else: b = b-a
+    return a+b
+
+def gcd_mod(a,b): 
+    while a != 0 and b != 0:
+        if a > b: a = a%b
+        else: b = b%a
+    return a+b
+
+def gcd_rec(a,b):
+    if a != 0 and b != 0:
+        if a > b: return gcd_rec(a%b,b) 
+        else: return gcd_rec(a,b%a)
+    else:
+        return a+b
+
+a = 5
+b = 7
+c = gcd_rec(a,b)
+print(c)
